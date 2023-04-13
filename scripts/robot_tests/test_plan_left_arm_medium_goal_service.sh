dfb63346651a8bae6f66393b1ea1e201e775cd56
Val <amstrudy@gmail.com>
2023-04-13 14:27:33 -0500

    more planning tests

---
+++ scripts/robot_tests/test_plan_left_arm_medium_goal_service.sh
@@ -0,0 +1,13 @@
+rosservice call /ValkyrieMoveItPlannerExecutorServerNode/plan_to_arm_goal "planning_group_arm: 2
+collision_aware_planning: true
+allowed_planning_time: 0.0
+arm_goal_pose:
+  position: 
+    x: -0.3030343336247427
+    y: 0.47331719980081277
+    z: 1.0406057717801371
+  orientation: 
+    x: 0.0057288622006614436
+    y: 0.023495923483701598
+    z: -0.13514603832142805
+    w: 0.9905304993000251"
---
+++ scripts/robot_tests/test_plan_right_arm_medium_goal_service.sh
@@ -0,0 +1,13 @@
+rosservice call /ValkyrieMoveItPlannerExecutorServerNode/plan_to_arm_goal "planning_group_arm: 1
+collision_aware_planning: true
+allowed_planning_time: 0.0
+arm_goal_pose:
+  position: 
+    x: 0.6933530696959437
+    y: 0.16503703732170694
+    z: 1.1042465841912528
+  orientation: 
+    x: 0.09208671750142892
+    y: 0.15143999076044717
+    z: 0.9760169842330906
+    w: 0.12639941513642663"
