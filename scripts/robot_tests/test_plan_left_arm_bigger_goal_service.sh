ac6d40569b84a5de30f53f95e5da20851f29d819
Val <amstrudy@gmail.com>
2023-04-13 10:43:41 -0500

    additional script for bigger arm goal

---
+++ scripts/robot_tests/test_plan_left_arm_bigger_goal_service.sh
@@ -0,0 +1,13 @@
+rosservice call /ValkyrieMoveItPlannerExecutorServerNode/plan_to_arm_goal "planning_group_arm: 2
+collision_aware_planning: true
+allowed_planning_time: 0.0
+arm_goal_pose:
+  position: 
+    x: 0.3616585330493876
+    y: 0.19856860938856205
+    z: 1.354061728081144
+  orientation: 
+    x: -0.1396211922893909
+    y: 0.025509096328352153
+    z: 0.9246790422477608
+    w: -0.35330422796219635"
